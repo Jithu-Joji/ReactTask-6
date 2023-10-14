f67d503a876feaada0ac002fbacc8a2991e16d7f
Jithu Joji <jithujoji@Jithus-MacBook-Air.local>
2023-10-14 23:03:40 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl": "https://github.com/Jithu-Joji",
+    "name": "Jithu Joji",
+    "role": "Full Stack Developer",
+    "linkedinId": "linkedin.com/in/jithu-joji/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks": [
+      {
+        "name": "linkedin",
+        "url": "https://linkedin.com/in/jithu-joji/",
+        "className": "fa fa-linkedin"
+      },
+      {
+        "name": "github",
+        "url": "https://github.com/Jithu-Joji",
+        "className": "fa fa-github"
+      },
+      {
+        "name": "skype",
+        "url": "",
+        "className": "fa fa-twitter"
+      }
+    ],
+    "aboutme": "Hello everyone! I'm currently a 3rd-year student pursuing Computer Science and Engineering with a strong foundation in C/C++, Python, and Java. I actively engage with the vibrant computer science community, connecting with like-minded individuals and exploring new technologies. In my free time, you'll often find me playing chess, solving Sudoku puzzles, or diving into books on personal growth and development. I am eager to learn and collaborate with others, and constantly on the lookout for new opportunities to expand my skillset and make meaningful contributions to the tech industry.",
+    "address": "Kerala, India",
+    "website": "https://github.com/Jithu-Joji",
+    "education": [
+      {
+        "UniversityName": "Vellore Institute of Technology",
+        "specialization": "B.tech Computer Science",
+        "MonthOfPassing": "",
+        "YearOfPassing": "2025",
+        "Achievements": "8.96 CGPA"
+      },
+      {
+        "UniversityName": "SVN-FIITJEE",
+        "specialization": "Higher Secondary",
+        "MonthOfPassing": "",
+        "YearOfPassing": "2021",
+        "Achievements": "12th Score 92.4%"
+      }
+      
+    ],
+    "work": [
+      {
+        "CompanyName": "Google Developers Students Club",
+        "specialization": "Competitive Programming",
+        "MonthOfLeaving": "2022",
+        "YearOfLeaving": "",
+      },
+      
+    ],
+    "skillsDescription": "Your skills here",
+    "skills": [
+      {
+        "skillname": "HTML5"
+      },
+      {
+        "skillname": "CSS"
+      },
+      {
+        "skillname": "Reactjs"
+      }
+    ],
+    "portfolio": [
+      {
+        "name": "SQL Injection prevention system",
+        "description": "mobile app",
+        "imgurl": "images/portfolio/project1.jpg"
+      },
+      {
+        "name": "Grocery Vendors Dashboard",
+        "description": "mobile app",
+        "imgurl": "images/portfolio/project2.jpg"
+      },
+      {
+        "name": "Fun site for Dogs",
+        "description": "mobile app",
+        "imgurl": "images/portfolio/project3.jpg"
+      },
+      {
+        "name": "Calculator",
+        "description": "mobile app",
+        "imgurl": "images/portfolio/project4.jpg"
+      }
+    ],
+    "testimonials": [
+      {
+        "description": "This is a sample testimonial",
+        "name": "Some technical guy"
+      },
+      {
+        "description": "This is a sample testimonial",
+        "name": "Some technical guy"
+      }
+    ]
+  };
+  
+  export default resumeData;
+  
